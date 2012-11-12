740120ffcc54d9ce24e3e7c7b2797728fd53b833
Jack <jack@donebecause.co.uk>
2012-11-12 08:18:01 +0000

    added social icons

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
 			<footer class="footer" role="contentinfo">
 			
 				<div id="inner-footer" class="wrap clearfix">
+					<div class="sharing-module">
+						<ul class="sharing-links">
+							<li class="sharing twitter"> <a href="">Twitter</a></li>
+							<li class="sharing facebook"> <a href="">Facebook</a></li>
+							<li class="sharing youtube"> <a href="">Youtube</a></li>
+							<li class="sharing vimeo"> <a href="">Vimeo</a></li>
+							<li class="sharing instagram"> <a href="">Instagram</a></li>
+							<li class="sharing rss"> <a href="">RSS</a></li>
+							<li class="sharing soundcloud"> <a href="">Soundcloud</a></li>
+						</ul>	
+					</div>
 					
 					<nav role="navigation">
     					<?php bones_footer_links(); ?>
